77dea24c7284e0c16ea957ebdaebcdcfb42d0058
xxxephyr <78284877+xxxephyr@users.noreply.github.com>
2021-01-30 19:12:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import discord
+import os
 from ics import Calendar, Event
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}.format'(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('!events'):
+    await message.channel.send('Command test successful')
+
+client.run(os.getenv('TOKEN'))
