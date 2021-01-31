c3d75309da9743b9f9bf8b021494028a8e73b194
xxxephyr <78284877+xxxephyr@users.noreply.github.com>
2021-01-30 22:55:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def on_message(message):
     await message.channel.send(get(msg)[0])
 
 client.run('ODA0OTE3NTA5MjQ4OTA5MzIz.YBTT2A.-OtAWlAdyJSXQeA14eW-2TUfYek')
-=======
+
     await message.channel.send(get(msg))
 
 client.run(os.getenv('TOKEN'))
